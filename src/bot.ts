cc284d890d6291d0f0a24ea5715ca6bf510618c4
toga4 <81744248+toga4@users.noreply.github.com>
2023-12-17 17:24:24 +0900

    extract interface for chat with api

--- src/bot.ts
+++ src/bot.ts
@@ -1,9 +1,5 @@
 import './fetch-polyfill'
-import {
-  VertexAI,
-  ChatSession,
-  GenerateContentResult
-} from '@google-cloud/vertexai'
+import {VertexAI, ChatSession} from '@google-cloud/vertexai'
 import {info, warning} from '@actions/core'
 // import pRetry from 'p-retry'
 import {VertexAIOptions, Options} from './options'
@@ -15,15 +11,69 @@ export interface Ids {
 }
 
 export class Bot {
-  private readonly api: ChatSession
+  private readonly api: ChatAPI
   private readonly options: Options
 
   constructor(options: Options, vertexaiOptions: VertexAIOptions) {
     this.options = options
+    this.api = new VertexAIAPI(options, vertexaiOptions)
+  }
+
+  chat = async (message: string): Promise<string> => {
+    try {
+      return await this.chat_(message)
+    } catch (e: unknown) {
+      if (e instanceof Error) {
+        warning(`Failed to chat: ${e}, backtrace: ${e.stack}`)
+      } else {
+        warning(`Failed to chat: ${e}`)
+      }
+      return ''
+    }
+  }
+
+  private readonly chat_ = async (message: string): Promise<string> => {
+    // record timing
+    const start = Date.now()
+    if (!message) {
+      return ''
+    }
+    info('----- MESSAGE START -----\n${message}\n----- MESSAGE END -----')
+    // response = await pRetry(
+    //   async () => {
+    //     return this.api!.sendMessage(message)
+    //   },
+    //   {retries: this.options.vertexaiRetries}
+    // )
+    const responseText = await this.api!.sendMessage(message)
+    const duration = Date.now() - start
+    info(`response time (including retries): ${duration} ms`)
+    if (!responseText) {
+      warning('vertexai response is empty')
+    }
+    return responseText
+  }
+}
+
+declare interface ChatAPI {
+  sendMessage(message: string): Promise<string>
+}
+
+class VertexAIAPI implements ChatAPI {
+  private readonly options: Options
+  private readonly chatSession: ChatSession
+
+  constructor(options: Options, vertexaiOptions: VertexAIOptions) {
+    this.options = options
+    const systemMessage = `${options.systemMessage}
+IMPORTANT: Entire response must be in the language with ISO code: ${options.language}
+`
+
     const vertexAI = new VertexAI({
       project: options.vertexaiProjectID,
       location: options.vertexaiLocation
     })
+
     const generativeModel = vertexAI.preview.getGenerativeModel({
       model: vertexaiOptions.model,
       // eslint-disable-next-line camelcase
@@ -38,10 +88,7 @@ export class Bot {
       }
     })
 
-    const systemMessage = `${options.systemMessage}
-IMPORTANT: Entire response must be in the language with ISO code: ${options.language}
-`
-    this.api = generativeModel.startChat({
+    this.chatSession = generativeModel.startChat({
       history: [
         {role: 'user', parts: [{text: systemMessage}]},
         {role: 'model', parts: [{text: options.replyForSystemMessage}]}
@@ -49,62 +96,11 @@ IMPORTANT: Entire response must be in the language with ISO code: ${options.lang
     })
   }
 
-  chat = async (message: string): Promise<string> => {
-    try {
-      return await this.chat_(message)
-    } catch (e: unknown) {
-      if (e instanceof Error) {
-        warning(`Failed to chat: ${e}, backtrace: ${e.stack}`)
-      }
-      return ''
-    }
-  }
-
-  private readonly chat_ = async (message: string): Promise<string> => {
-    // record timing
-    const start = Date.now()
-    if (!message) {
-      return ''
-    }
-
-    info(`----- MESSAGE START -----
-${message}
------ MESSAGE END -----`)
-    let response: GenerateContentResult | undefined
-
-    try {
-      // response = await pRetry(
-      //   async () => {
-      //     return this.api!.sendMessage(message)
-      //   },
-      //   {retries: this.options.vertexaiRetries}
-      // )
-      response = await this.api!.sendMessage(message)
-    } catch (e: unknown) {
-      if (e instanceof Error) {
-        warning(
-          `failed to send message to vertexai: ${e}, backtrace: ${e.stack}`
-        )
-      } else {
-        warning(`failed to send message to vertexai: ${e}`)
-      }
-    }
-    const end = Date.now()
-    info(`response: ${JSON.stringify(response, null, 2)}`)
-    info(
-      `vertexai sendMessage (including retries) response time: ${
-        end - start
-      } ms`
-    )
-    let responseText = ''
-    if (response != null) {
-      responseText = response.response.candidates[0].content.parts[0].text || ''
-    } else {
-      warning('vertexai response is null')
-    }
+  async sendMessage(message: string): Promise<string> {
+    const result = await this.chatSession.sendMessage(message)
     if (this.options.debug) {
-      info(`vertexai responses: ${responseText}`)
+      info(`response: ${JSON.stringify(result, null, 2)}`)
     }
-    return responseText
+    return result.response.candidates[0].content.parts[0].text || ''
   }
 }
